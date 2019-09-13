d52de11eb8d40d3037d095286e6fa4e53e14dfba
Amandeep Kirar <amandeep.kirar@live.com>
2019-09-13 00:47:38 -0400

    Array | Better code for min Value handling in HourGlassSum

--- HackerRank/Arrays.cs
+++ HackerRank/Arrays.cs
@@ -11,14 +11,14 @@ namespace HackerRank
         {
             if (arr == null || arr.Length != 6) throw new ArgumentException();
 
-            int maxHrGlass = 0;
+            int maxHrGlass = Int32.MinValue;
             int currentHrGlassSum = 0;
             for (int i = 1; i <= 4; i++) {
                 for (int j = 1; j <= 4; j++) {
 
                     currentHrGlassSum = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
 
-                    if ((i==1 && j==1) || currentHrGlassSum > maxHrGlass) maxHrGlass = currentHrGlassSum;
+                    if (currentHrGlassSum > maxHrGlass) maxHrGlass = currentHrGlassSum;
                 }
             }
 
