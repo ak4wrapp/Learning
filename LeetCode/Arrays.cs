9a86b16a68557d4f06d927e55b06959de7ed8c14
ak4wrapp <ak4wrapp@gmail.com>
2021-01-31 00:55:39 -0500

    IsPalindrome

--- LeetCode/Arrays.cs
+++ LeetCode/Arrays.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace LeetCode
 {
-    public class ArraysTests
+    public class Arrays
     {
         #region RemoveDuplicates
         public static int RemoveDuplicates(int[] nums)
@@ -195,6 +195,63 @@ namespace LeetCode
             }
         }
 
+        #endregion
+
+        #region Find all duplicates in Array
+        /*
+         *  https://leetcode.com/problems/find-all-duplicates-in-an-array/
+            Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+            Find all the elements that appear twice in this array.
+
+            Could you do it without extra space and in O(n) runtime?
+
+            Example:
+            Input:
+            [4,3,2,7,8,2,3,1]
+
+            Output:
+            [2,3]
+        */
+
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            /*  How we plan to solve it
+             *  
+             *  We know numbers are between range of 1 to length of array
+             *  We will browse througha all numbers and flag each number at i-1 index as negative
+             *  netx time when same number appear we will find index (i-1) as negative number
+             *  so we will know this number is duplicate
+             */
+            List<int> duplicateNumbers = new List<int>();
+            if (nums == null || nums.Length == 0)
+                return duplicateNumbers;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                // Check if number at index i is already found and has a value less than 0
+                if (nums[index] < 0)
+                {
+                    // number was updated earlier hence its a duplicate one this time
+                    duplicateNumbers.Add(index + 1);
+                }
+
+                nums[index] = -nums[index];
+            }
+            return duplicateNumbers;
+        }
+
+        [Test]
+        public void FindDuplicatesTest()
+        {
+            foreach (var spec in new[]{
+                ( Input: new List<int>(){4,3,2,7,8,2,3,1}, ExpectedOutPut: new List<int>(){2,3 } )
+            })
+            {
+                var result = FindDuplicates(spec.Input.ToArray());
+                Assert.AreEqual(spec.ExpectedOutPut, result); ;
+            }
+        }
         #endregion
 
     }
---
+++ LeetCode/Strings.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    [TestFixture]
+    public class Strings
+    {
+        #region Check is Palindrome
+
+        /*  https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+         *  
+         *  Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+            Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+            Example 1:
+
+                Input: "A man, a plan, a canal: Panama"
+                Output: true
+
+            Example 2:
+
+                Input: "race a car"
+                Output: false
+
+            Constraints:
+
+            s consists only of printable ASCII characters.
+         */
+
+
+        public bool IsPalindrome(string s)
+        {
+            int leftPositionIndex = 0;
+            int rightPostionIndex = s.Length - 1;
+
+            while (leftPositionIndex < rightPostionIndex)
+            {
+                if (!Char.IsLetterOrDigit(s[leftPositionIndex]))
+                {
+                    leftPositionIndex++;
+                    continue;
+                }
+
+                if (!Char.IsLetterOrDigit(s[rightPostionIndex]))
+                {
+                    rightPostionIndex--;
+                    continue;
+                }
+
+                if (Char.ToLower(s[leftPositionIndex]) != Char.ToLower(s[rightPostionIndex]))
+                {
+                    return false;
+                }
+
+                leftPositionIndex++;
+                rightPostionIndex--;
+            }
+            return true;
+        }
+
+        [Test]
+        public void IsPalindromeTest()
+        {
+            foreach (var spec in new[] {
+                (input: "A man, a plan, a canal: Panama", output: true),
+                (input: "race a car", output: false)
+            })
+            {
+                Assert.AreEqual(spec.output, IsPalindrome(spec.input));
+            }
+        }
+
+        #endregion
+    }
+}
