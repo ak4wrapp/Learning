09ed1fecb7c5daaecd343a22687dc8d496b81cd4
Amandeep Kirar <amandeep.kirar@live.com>
2021-01-31 16:41:48 -0500

    ReverseString

--- LeetCode/Strings.cs
+++ LeetCode/Strings.cs
@@ -73,5 +73,59 @@ namespace LeetCode
         }
 
         #endregion
+
+        #region Reverse String
+
+        /*
+         *  Write a function that reverses a string. The input string is given as an array of characters char[].
+
+            Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+            You may assume all the characters consist of printable ascii characters.
+
+            Example 1:
+
+            Input: ['h','e','l','l','o']
+            Output: ['o','l','l','e','h']
+            
+            Example 2:
+
+            Input: ['H','a','n','n','a','h']
+            Output: ['h','a','n','n','a','H']
+
+            Hint #1  
+            
+            The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+         */
+
+        public void ReverseString(char[] s)
+        {
+            int index = 0;
+            int midIndex = s.Length / 2;
+
+            for (int i = s.Length -1; i >= midIndex; i--)
+            {
+                char c = s[i];
+                s[i] = s[index];
+                s[index++] = c;
+            }
+        }
+
+        [Test]
+        public void ReverseStringTest()
+        {
+            foreach (var spec in new[]{
+                ( Input: new char[] { 'h','e','l','l','o'}, ExpectedOutPut: new int[] { 'o', 'l', 'l', 'e', 'h'}),
+                ( Input: new char[] { 'H','a','n','n','a','h'}, ExpectedOutPut: new int[] { 'h','a','n','n','a','H'})
+            })
+            {
+                ReverseString(spec.Input);
+
+                Assert.AreEqual(spec.ExpectedOutPut, spec.Input);
+            }
+        }
+
+
+        #endregion
     }
 }
