cbdf71239b0bf7a3ca70bce1e690bb3c630d39a2
ak4wrapp <ak4wrapp@gmail.com>
2021-02-07 11:03:17 -0500

    Merge Sorted Lists

--- LeetCode/LinkedList.cs
+++ LeetCode/LinkedList.cs
@@ -157,5 +157,75 @@ namespace LeetCode
         }
 
         #endregion
+
+
+        #region 21. Merge Two Sorted Lists
+
+        #region Problem Statement
+
+        /*  
+         *  https://leetcode.com/problems/merge-two-sorted-lists/
+         *  
+         *  Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+            Example 1:
+
+            Input: l1 = [1,2,4], l2 = [1,3,4]
+            Output: [1,1,2,3,4,4]
+
+            Example 2:
+
+            Input: l1 = [], l2 = []
+            Output: []
+
+            Example 3:
+
+            Input: l1 = [], l2 = [0]
+            Output: [0]
+         */
+        #endregion
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            var runnerHead = new ListNode(-1);   // Creating this dummy node ease the logic
+            var newHead = runnerHead;           // This is what would be merged List
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val >= l2.val)
+                {
+                    runnerHead.next = l2;
+                    l2 = l2.next;
+                }
+                else
+                {
+                    runnerHead.next = l1;
+                    l1 = l1.next;
+                }
+
+                runnerHead = runnerHead.next;
+            }
+
+            // Simply add the 'leftover' from the while loop at the end 
+            if (l1 != null) runnerHead.next = l1;
+            if (l2 != null) runnerHead.next = l2;
+
+            return newHead.next;
+        }
+
+
+        [Test]
+        public void MergeTwoListsTest()
+        {
+            ListNode InputLinkedList1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            ListNode InputLinkedList2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+            ListNode output = MergeTwoLists(InputLinkedList1, InputLinkedList2);
+
+            Assert.IsTrue(true);
+        }
+        #endregion
     }
 }
